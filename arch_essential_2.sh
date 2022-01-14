7263c5d1e6e63caf948dd3b7a77f14879dcbec08
fots <facundotomatis@gmail.com>
2022-01-13 23:08:14 -0300

    removing swapfile if already created

--- arch_essential_2.sh
+++ arch_essential_2.sh
@@ -26,20 +26,30 @@ read swap_size
 if ! [[ $swap_size =~ ^[0-9]+[MG]B$ ]]; then
         swap_size="4GB"
 fi
-echo $swap_size
+[[ -f /swapfile ]] && swapoff /swapfile 2>/dev/null || rm /swapfile
 fallocate -l "$swap_size" /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab.bak > /dev/null
+echo -e "-----------------$(clrg "CHECK FSTAB")-----------------"
 cat /etc/fstab.bak
+echo -e "^^^^^^^^^^^^^^^^^$(clrg "CHECK FSTAB")^^^^^^^^^^^^^^^^^"
 echo -en "Everything ok? [Y/n]: "
 read ok
 [[ $ok =~ ^n+$ ]] && exit $E_NCON
 mv /etc/fstab.bak /etc/fstab
 
 # Setting timezone
-ln -sf /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
+echo -e "For a extended list of timezones execute $(clrg "ls /usr/share/zoneinfo")"
+echo -en "Choose a timezone. Default: $(clrg "America/Argentina/Buenos_Aires"): "
+read timezone
+[[ $timezone = "" ]] && timezone="America/Argentina/Buenos_Aires"
+echo "Path of timezone: /usr/share/zoneinfo/$timezone"
+echo -en "Everything ok? [Y/n]: "
+read ok
+[[ $ok =~ ^n+$ ]] && exit $E_NCON
+ln -sf "/usr/share/zoneinfo/$timezone" /etc/localtime
 hwclock --systohc
 
 # Setting language locale
@@ -48,6 +58,7 @@ read locale
 [[ $locale = "" ]] && locale="en_US"
 loc=$(grep "${locale}.UTF-8" /etc/locale.gen | tail -1 | awk -F "#" '{print $2}')
 awk -v loc="#${loc}" -F"#" '$0~loc {print $2} {print}' /etc/locale.gen > /etc/locale.gen.bak
+echo "$(clrg "locale.gen") content"
 awk -F "#" '! /^#/ {print}' /etc/locale.gen.bak
 echo -en "Everything ok? [Y/n]: "
 read ok
@@ -55,6 +66,7 @@ read ok
 mv /etc/locale.gen.bak /etc/locale.gen
 locale-gen
 echo "LANG=$locale.UTF-8" > /etc/locale.conf.bak
+echo "$(clrg "locale.conf") content"
 cat /etc/locale.conf.bak
 echo -en "Everything ok? [Y/n]: "
 read ok
@@ -68,9 +80,11 @@ echo "$host_name" > /etc/hostname
 echo -e "\n127.0.0.1 localhost\n::1 localhost\n127.0.1.1 $host_name" >> /etc/hosts
 
 # Setting password
+echo "Setting a new password"
 passwd
 
 # Setting bootloader
+echo "Setting grub"
 pacman -S grub efibootmgr os-prober
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -79,7 +93,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 
 # Last steps!
-echo -en "$(clrg "Last steps!")"
-echo "type $(clrg "exit")"
-echo "then $(clrg "umount -a")"
-echo "and after that $(clrg "reboot")"
+echo -e "$(clrg "Last steps!")"
+echo -e "type $(clrg "exit")"
+echo -e "then $(clrg "umount -a")"
+echo -e "and after that $(clrg "reboot")"
